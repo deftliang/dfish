7a6ee2f301fdb09e27708dd79d9318de760bc4ba
deft <deftmail@163.com>
2020-10-17 21:29:58 +0800

    finished Menu component and it's test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 // import Button, { ButtonType, ButtonSize } from './components/Button/button'
-import Alert, {AlertType} from './components/Alert/alert'
+// import Alert, {AlertType} from './components/Alert/alert'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
+import SubMenu from './components/Menu/subMenu'
 
 function App() {
   return (
     <div className="App">
+      <Menu
+        defaultIndex='0'
+        onSelect={(index) => {
+          alert(index)
+        }}
+        mode="vertical"
+        defaultOpenSubMenus={['2']}
+      >
+        <MenuItem>cool link 1</MenuItem>
+        <MenuItem disabled>cool link 2</MenuItem>
+        <SubMenu title="drapdown">
+          <MenuItem>dropdown 1</MenuItem>
+          <MenuItem>dropdown 2</MenuItem>
+          <MenuItem>dropdown 3</MenuItem>
+        </SubMenu>
+      </Menu>
+
       {/* <Button
         btnType={ButtonType.Danger}
         size={ButtonSize.Small}
@@ -30,11 +50,12 @@ function App() {
       <Button btnType={ButtonType.Link} href="https://www.baidu.com" disabled>
         baidu
       </Button> */}
-      <Alert title="提示" closable alertType={AlertType.Danger}>hello world</Alert>
+
+      {/* <Alert title="提示" closable alertType={AlertType.Danger}>hello world</Alert>
       <Alert title="提示" closable alertType={AlertType.Default}>hello world</Alert>
       <Alert title="提示" closable alertType={AlertType.Warning}>hello world</Alert>
       <Alert title="提示" closable alertType={AlertType.Success}>hello world</Alert>
-      <Alert closable></Alert>
+      <Alert closable></Alert> */}
     </div>
   )
 }
